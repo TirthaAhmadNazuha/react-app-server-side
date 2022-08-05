db4fd52a372b3b0d34c491edccb4e3f1d85b359c
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2022-08-05 21:50:18 +0700

    push to github

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.use(express.json())
 
@@ -7,6 +8,29 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.post('/post', (req, res) => {
+    console.log(req.body)
+    res.send(JSON.stringify(req.body))
+})
+app.post('/signup', (req, res) => {
+    const users = JSON.parse(fs.readFileSync('users.json', { encoding: 'utf8', flag: 'r' }))
+    users.push(req.body)
+    fs.writeFileSync('users.json', JSON.stringify(users))
+    res.redirect('/')
+
+})
+app.post('/login', (req, res) => {
+    const users = JSON.parse(fs.readFileSync('users.json', { encoding: 'utf8', flag: 'r' }))
+    for (const user of users) {
+        if (user.username == req.body.username && user.password == req.body.password) {
+            const id = user.id
+            const name = user.name
+            const username = user.username
+            const email = user.email
+            res.send(JSON.stringify({ id, name, username, email }))
+        }
+    }
+})
+app.post('/imgavataruser', (req, res) => {
     console.log(req.body)
     res.send(JSON.stringify(req.body))
 })
